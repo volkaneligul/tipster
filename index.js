1d32eb11b3b88eb8435b8954de904b1497b69e62
Volkan Eligül <volkan.eligul@hepsiburada.com>
2017-08-11 14:09:51 +0300

    add-on mlab

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
-var express =  require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+// set up express app
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring = 
+  process.env.MONGODB_URI || 
+  'mongodb://localhost/ninjago';
+
+// connect to mongodb
+//mongoose.connect('mongodb://localhost/ninjago');
+//mongoose.connect('mongodb://heroku_cd8j2zbv:a3pci2krism8oqcun4bgs3e7am@ds161262.mlab.com:61262/heroku_cd8j2zbv', {
+//    useMongoClient: true
+//})
+//mongoose.Promise = global.Promise;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+/* operations and release them when the connection is complete.
+mongoose.connect(uristring, { useMongoClient: true },  function (err, res) {
+    if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+    console.log ('Succeeded connected to: ' + uristring);
+    }
+});
+mongoose.Promise = global.Promise;
+*/
+
+
+//set up static files
+app.use(express.static('public'));
+
+// use body-parser middleware
+app.use(bodyParser.json());
+
+// initialize routes
+app.use('/api', require('./routes/api'));
+
+// error handling middleware
+app.use(function(err, req, res, next){
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 });
 
-app.get('/users', function(req, res) {
-  // Hard coding for simplicity. Pretend this hits a real database
-  res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-  ]);
-});
-
+// listen for requests
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port')); 
 });
