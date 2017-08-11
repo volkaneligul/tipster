2e6ad846c42425d88ee08d29aabfd05836abdc4f
Volkan Eligül <volkan.eligul@hepsiburada.com>
2017-08-11 14:26:53 +0300

    add-on mlab

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ var uristring =
 //})
 //mongoose.Promise = global.Promise;
 
-// Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
+// connect to mongodb
 mongoose.connect(uristring, { useMongoClient: true },  function (err, res) {
     if (err) {
     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
