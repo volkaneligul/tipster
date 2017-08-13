7f8a9d1c8e1deddb1cd6cc31faf743cc4c3df274
Volkan Eligül <volkaneligul@gmail.com>
2017-08-13 16:48:13 +0300

    user authentication

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+// set up express app
+const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// set up express app
-const app = express();
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
 var uristring = 
   process.env.MONGODB_URI || 
-  'mongodb://localhost/ninjago';
+  'mongodb://localhost:27017/tipster';
 
 // connect to mongodb
 //mongoose.connect('mongodb://localhost/ninjago');
@@ -27,15 +30,34 @@ mongoose.connect(uristring, { useMongoClient: true },  function (err, res) {
     }
 });
 mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+});
+
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
 
 //set up static files
 app.use(express.static('public'));
 
 // use body-parser middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // initialize routes
 app.use('/api', require('./routes/api'));
+app.use('/user', require('./routes/user'));
 
 // error handling middleware
 app.use(function(err, req, res, next){
---
+++ routes/user.js
@@ -0,0 +1,37 @@
+const express = require ('express');
+const router = express.Router();
+const User = require('../models/user');
+
+// GET route after registering
+router.get('/profile', function (req, res, next) {
+  User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        if (user === null) {
+          var err = new Error('Not authorized! Go back!');
+          err.status = 400;
+          return next(err);
+        } else {
+          return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/user/logout">Logout</a>')
+        }
+      }
+    });
+});
+
+// GET for logout logout
+router.get('/logout', function (req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+module.exports = router;
